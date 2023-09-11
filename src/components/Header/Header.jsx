d9f2e278f3eecbb8eadb3d9c77aae30d0f20be10
Habibur Rahman <habib.swi16@gmail.com>
2023-09-11 18:39:51 +0600

    header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="bg-sky-300">
+      <h1 className="text-4xl font-bold  p-10 text-center rounded-xl shadow-2xl w-11/12 mx-auto">
+        Knowledge Cafe
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
