41de08e6b33346118d0cd1c0422d9067d772dae7
Habibur Rahman <habib.swi16@gmail.com>
2023-09-11 18:46:43 +0600

    fetch data

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("blogs.json");
+      const data = await res.json();
+      setBlogs(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+};
+
+export default Blogs;
